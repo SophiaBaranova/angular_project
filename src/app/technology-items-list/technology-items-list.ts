c92b6162ad67dcdc034d43c8a69268cffff68a33
Sofiia Baranova <sonya.brnv@gmail.com>
2025-10-12 22:45:16 +0300

    Implement search function

--- src/app/technology-items-list/technology-items-list.ts
+++ src/app/technology-items-list/technology-items-list.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TechnologyItem } from '../core/models/technology-item.model';
 import { TechnologyItemCard } from '../technology-item-card/technology-item-card';
 import { TECHNOLOGY_ITEMS } from '../mock-data/technology-items';
+import {Search} from '../search/search';
 
 @Component({
   selector: 'app-technology-items-list',
   standalone: true,
-  imports: [CommonModule, TechnologyItemCard],
+  imports: [CommonModule, TechnologyItemCard, Search],
   templateUrl: './technology-items-list.html',
   styleUrl: './technology-items-list.scss',
 })
 export class TechnologyItemsList {
   items: TechnologyItem[] = TECHNOLOGY_ITEMS;
+  searchText!: string;
+  search(searchText: string){
+    this.searchText = searchText.trim().toLowerCase();
+  }
+  get filteredItems(){
+    if (!this.searchText) return this.items;
+    return this.items.filter(item => item.name.toLowerCase().includes(this.searchText));
+  }
   showDetails(item: TechnologyItem) {
     console.log(
       `Details:
