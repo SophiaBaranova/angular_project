956a0781fcd9c992a78d85269f94b09f0f75db89
Sofiia Baranova <sonya.brnv@gmail.com>
2025-11-21 20:14:53 +0300

    Add route /TechnologyItems/create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { TechnologyItemsList } from './technology-items-list/technology-items-list';
 import { TechnologyItemDetails } from './technology-item-details/technology-item-details';
+import { TechnologyItemForm } from './forms/technology-item-form/technology-item-form';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'TechnologyItems', pathMatch: 'full' },
     { path: 'TechnologyItems', component: TechnologyItemsList },
-    { path: 'TechnologyItems/:id', component: TechnologyItemDetails }
+    { path: 'TechnologyItems/create', component: TechnologyItemForm },
+    { path: 'TechnologyItems/:id', component: TechnologyItemDetails },
 ];
