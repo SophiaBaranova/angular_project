8152fd65cd3275db6df151913d0dee603c44e023
Sofiia Baranova <sonya.brnv@gmail.com>
2025-12-14 21:33:04 +0300

    Add auth-token.interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,14 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { baseUrlInterceptor } from './interceptors/base-url.interceptor';
+import { authTokenInterceptor } from './interceptors/auth-token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(
-      withInterceptors([baseUrlInterceptor])
+      withInterceptors([baseUrlInterceptor, authTokenInterceptor])
     ),
     provideRouter(routes)
   ]
