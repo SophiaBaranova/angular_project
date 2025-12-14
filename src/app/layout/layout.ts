36fdfc3fb087377a33710872e111fbf8981213ed
Sofiia Baranova <sonya.brnv@gmail.com>
2025-12-14 21:32:01 +0300

    Change layout component to display items list only if user is logged in

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '../services/auth.service/auth.service';
+import { Observable } from 'rxjs';
+import { Login } from "../login/login";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AsyncPipe, RouterLink, Login],
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
 export class Layout {
   appName = 'Web Development';
+  isAuthenticated$!: Observable<boolean>;
+  constructor(private authService: AuthService) {
+    this.isAuthenticated$ = this.authService.isAuthenticated$;
+  }
+  logout() {
+    this.authService.logout();
+  }
 }
