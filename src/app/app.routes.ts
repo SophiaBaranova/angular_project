13a77cb3b1881aead458b2e8e24cdacbf22bf7cb
Sofiia Baranova <sonya.brnv@gmail.com>
2025-12-14 21:33:56 +0300

    Add auth.guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { TechnologyItemsList } from './technology-items-list/technology-items-list';
 import { TechnologyItemDetails } from './technology-item-details/technology-item-details';
 import { TechnologyItemForm } from './forms/technology-item-form/technology-item-form';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'TechnologyItems', pathMatch: 'full' },
-    { path: 'TechnologyItems', component: TechnologyItemsList },
-    { path: 'TechnologyItems/create', component: TechnologyItemForm },
-    { path: 'TechnologyItems/:id', component: TechnologyItemDetails },
+    { path: 'TechnologyItems', component: TechnologyItemsList, canActivate: [AuthGuard] },
+    { path: 'TechnologyItems/create', component: TechnologyItemForm, canActivate: [AuthGuard] },
+    { path: 'TechnologyItems/:id', component: TechnologyItemDetails, canActivate: [AuthGuard] },
 ];
