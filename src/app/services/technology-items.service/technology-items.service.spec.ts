9cd36f997c448ea35599e9f160a3f716b2bcbcba
Sofiia Baranova <sonya.brnv@gmail.com>
2025-12-19 20:01:18 +0300

    Add unit-test for technology-items.service

--- src/app/services/technology-items.service/technology-items.service.spec.ts
+++ src/app/services/technology-items.service/technology-items.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TechnologyItemsService } from './technology-items.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TECHNOLOGY_ITEMS } from '../../mock-data/technology-items';
 
 describe('TechnologyItemsService', () => {
   let service: TechnologyItemsService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TechnologyItemsService]
+    });
+
     service = TestBed.inject(TechnologyItemsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+
+    // Mock the initial GET call from loadItems() in constructor
+    const req = httpTestingController.expectOne('items.json');
+    expect(req.request.method).toBe('GET');
+
+    // Simulate response
+    const responseData: Record<string, any> = {};
+    TECHNOLOGY_ITEMS.forEach(item => {
+      const { id, ...rest } = item;
+      responseData[id] = rest;
+    });
+    req.flush(responseData);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should retrieve item by ID from items$', () => {
+    service.getItemById('1').subscribe(item => {
+      const expected = { ...TECHNOLOGY_ITEMS[0], logo: 'assets/' + TECHNOLOGY_ITEMS[0].logo };
+      expect(item).toEqual(expected);
+    });
   });
 });
